16ec5a6dc7f3916dd58ff9716deed074ec96c5a7
PuriThanapon <purithanapon@gmail.com>
2025-08-31 06:42:48 +0700

    menu

--- frontend/src/app/components/Menu.tsx
+++ frontend/src/app/components/Menu.tsx
@@ -25,7 +25,7 @@ const menuItems = [
       { icon: <Users size={20} />, label: "รายชื่อผู้ป่วย", href: "/patient" },
       { icon: <Stethoscope size={20} />, label: "ข้อมูลการรักษา", href: "/treatment" },
       { icon: <CalendarDays size={20} />, label: "การนัดหมาย", href: "/appointments" },
-      { icon: <Bed size={20} />, label: "จัดการเตียง", href: "bed-stays"},
+      { icon: <Bed size={20} />, label: "จัดการเตียง", href: "/bed-stays"},
       { icon: <FileText size={20} />, label: "ออกรายงาน", href: "/report" },
       { icon: <FolderClosed size={20} />, label: "เอกสาร", href: "/templates"},
     ],
